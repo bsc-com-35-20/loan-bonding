8618976762a6576442a5948bb66c0d224aa6eea7
bed-com-36-20 <Bed-com-36-20@unima.ac.mw>
2023-12-09 00:03:42 +0200

    modified page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import images.jpg from `../components/images.jpg`;
 
 function Home() {
   return (
-    <div>Home</div>
-  )
-}
+  <div className="container">
+    <Head>
+      <h1>Welcome to the Student Loan Bonding Application!</h1>
+    </Head>
+    <main>
+      <section className="hero">
+        <h2>finish the last step of the loan process so you can receive your loan and start enjoying its benefits</h2>
+        <a href="/application" className="btn">Start Bonding</a>
+      </section>
+      <section className="how-to-bond">
+        <h2>How to Bond for your Loan</h2>
+        <ol>
+          <li>Create an account (if you haven't already).</li>
+        </ol>
+      </section>
+    </main>
+    <footer>
+      <p> Student Loan Bonding Application</p>
+      <nav>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+      </nav>
+    </footer>
+  </div>
+);
+  }
 
-export default Home
+export default Home;
