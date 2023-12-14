ca7aa02784d186dfddff915d4d7e831aee6fa50b
Kumbukani Msukwa <bed-com-20-20@unima.ac.mw>
2023-12-13 17:51:56 -0800

    validation on personal information

--- src/app/componets/PersonalInfor.tsx
+++ src/app/componets/PersonalInfor.tsx
@@ -1,121 +1,133 @@
-"use client"
-import { useForm } from "react-hook-form"
-import { useFormState } from "./FormContext";
-import './form.css';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useFormState } from './FormContext';
 
 type TFormValues = {
-   surname:string;
-   firstname:string;
-   othername:string;
-   dateofbirth:string;
-   sex:string;
-   home:string;
-   phonenumber:string;
+  surname: string;
+  firstname: string;
+  othername: string;
+  dateofbirth: string;
+  sex: string;
+  home: string;
+  phonenumber: string;
+  email: string;
 };
-export function PersonalInfor(){
-    const { onHandleNext } = useFormState();
 
-    const { register,handleSubmit} = useForm<TFormValues>();
-    
-    function onHandleFormSubmit(data:TFormValues){
-      console.log(data);
-        onHandleNext();
+export function PersonalInfo() {
+  const { onHandleNext } = useFormState();
+  const { register, handleSubmit } = useForm<TFormValues>();
+  const [error, setError] = useState<boolean>(false);
+
+  const onHandleFormSubmit = (data: TFormValues) => {
+    console.log(data);
+
+    if (isFormValid(data)) {
+      // If all required fields are filled and valid, proceed to the next step
+      onHandleNext();
+    } else {
+      // If any required field is missing or invalid, show an error message
+      setError(true);
     }
+  };
 
-    return <form className="space-y-4 textdd w-full"  onSubmit={handleSubmit(onHandleFormSubmit)}>
-      <h1 className='text-xl font-semibold text-center'>Personal Information</h1>
-        <div className="flex flex-col  gap-1">
+  const isFormValid = (data: TFormValues) => {
+    return (
+      data.surname &&
+      data.firstname &&
+      isValidDate(data.dateofbirth) &&
+      data.sex &&
+      data.home &&
+      data.phonenumber &&
+      isValidEmail(data.email)
+    );
+  };
 
-        <label htmlFor="surname" >Surname</label>
+  const isValidDate = (dateString: string) => {
+    // You may want to use a library like 'date-fns' or 'moment' for more advanced date validation
+    return /^\d{4}-\d{2}-\d{2}$/.test(dateString);
+  };
+
+  const isValidEmail = (email: string) => {
+    // Simple email validation regex
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  return (
+    <form className="space-y-6 text-16" onSubmit={handleSubmit(onHandleFormSubmit)}>
+      <h1 className="text-xl font-semibold text-center">Personal Information</h1>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="surname">Surname</label>
         <input
-         id="surname"
-         type="text"
-         placeholder="surname"
-         className="h-10 px-4 border rounded-md"
-         {...register("surname")}
-          />
+          id="surname"
+          type="text"
+          placeholder="Surname"
+          className="h-11 px-4 border rounded-md"
+          {...register('surname')}
+        />
+      </div>
 
-        </div>
-        <div className="flex flex-col gap-1">
-
-        <label htmlFor="firstname" >First Name</label>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="firstname">First Name</label>
         <input
-         id="firstname"
-         type="text"
-         placeholder="First Name"
-         className="h-10  px-4 border rounded-md"
-         {...register("firstname")}
-          />
-         
-         </div>
-        <div className="flex flex-col gap-1">
+          id="firstname"
+          type="text"
+          placeholder="First Name"
+          className="h-11  px-4 border rounded-md"
+          {...register('firstname')}
+        />
+      </div>
 
-        
-        <label htmlFor="othername" >Other name</label>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="dateofbirth">Date Of Birth</label>
         <input
-         id="othername"
-         type="text"
-         placeholder="Other Name"
-         className="h-10  px-4 border rounded-md"
-         {...register("othername")}
-          />
+          id="dateofbirth"
+          type="date"
+          placeholder="Date Of Birth"
+          className="h-11  px-4 border rounded-md"
+          {...register('dateofbirth')}
+        />
+      </div>
 
-        </div>
-        <div className="flex flex-col gap-1">
-
-        
-        <label htmlFor="dateofbirth" >Date Of Birth</label>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="sex">Sex</label>
         <input
-         id="dateofbirth"
-         type="date"
-         placeholder="Date Of Birth"
-         className="h-10  px-4 border rounded-md"
-         {...register("dateofbirth")}
-          />
+          id="sex"
+          type="text"
+          placeholder="Sex"
+          className="h-11  px-4 border rounded-md"
+          {...register('sex')}
+        />
+      </div>
 
-        </div>
-        <div className="flex flex-col gap-1">
-
-        
-        <label htmlFor="sex" >Sex</label>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="phonenumber">Phone Number</label>
         <input
-         id="sex"
-         type="text"
-         placeholder="Sex"
-         className="h-10  px-4 border rounded-md"
-         {...register("sex")}
-          />
+          id="phonenumber"
+          type="text"
+          placeholder="Phone Number"
+          className="h-11  px-4 border rounded-md"
+          {...register('phonenumber')}
+        />
+      </div>
 
-        </div>
-        <div className="flex flex-col gap-1">
-
-        
-        <label htmlFor="home" >Home village/Traditional Authority/District</label>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="home">Home Village/Traditional Authority/District</label>
         <input
-         id="home"
-         type="text"
-         placeholder="Home village/Traditional Authority/District"
-         className="h-10  px-4 border rounded-md"
-         {...register("home")}
-          />
+          id="home"
+          type="text"
+          placeholder="Home Village/Traditional Authority/District"
+          className="h-11  px-4 border rounded-md"
+          {...register('home')}
+        />
+      </div>
 
-<div className="flex flex-col gap-2">
-
-        
-<label htmlFor="phonenumber">Phone Number</label>
-<input
- id="phonenumber"
- type="text"
- placeholder="Phone Number"
- className="h-10  px-4 border rounded-md"
- {...register("phonenumber")}
-  />
-
-</div>
-
-        </div>
-        <div className="flex justify-end">
-         <button className="h-11 px-6 bg-amber-600 text-white rounded-md">Next</button>
-        </div>
+      <div className="flex justify-end">
+        {error && <div style={{ color: 'red' }}>Please enter all required and valid information.</div>}
+        <button className="h-11 px-6 bg-amber-600 text-white rounded-md">Next</button>
+      </div>
     </form>
+  );
 }
+
+export default PersonalInfo;
