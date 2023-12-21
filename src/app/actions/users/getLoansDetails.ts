572a4f3f2d3aabe920780c627b566cd5a0b1cdbe
bsc-inf-01-20 <127177131+bsc-inf-01-20@users.noreply.github.com>
2023-12-21 06:47:48 +0200

    fixing schema

--- src/app/actions/users/getLoansDetails.ts
+++ src/app/actions/users/getLoansDetails.ts
@@ -1,4 +1,4 @@
-'user server'
+'use server'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import prisma from '@/app/lib/prisma';
 import { getServerSession } from 'next-auth';
@@ -12,7 +12,6 @@ export const getLoans = async () => {
         where: {
           email: session.user.email,
         },
-
       });
 
       if (getuser?.regNumber) {
@@ -20,16 +19,39 @@ export const getLoans = async () => {
           where: {
             regNumber: getuser.regNumber,
           },
-
         });
 
         const studentUpkeep = await prisma.upkeep.findUnique({
-            where: {
-                regNumber:getuser.regNumber
-            }
-        })
+          where: {
+            regNumber: getuser.regNumber,
+          },
+        });
 
-       
+        if (studentFees && studentUpkeep) {
+          // User is available in both tables
+          return {
+            success: true,
+            amount: 1000000,
+          };
+        } else if (studentFees) {
+          // User is available in fees table only
+          return {
+            success: true,
+            amount: 650000,
+          };
+        } else if (studentUpkeep) {
+          // User is available in upkeep table only
+          return {
+            success: true,
+            amount: 350000,
+          };
+        } else {
+          // User is not available in either fees or upkeep table
+          return {
+            success: false,
+            message: 'User not found in fees or upkeep table',
+          };
+        }
       } else {
         return { success: false, message: 'Registration number not found for the user' };
       }
